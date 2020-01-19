e118fde19ba7190dd7514d6e6a9e48fa1c9fae1e
Jasim Khan <jasim.khan89@gmail.com>
2020-01-18 23:05:11 -0500

    added paragraph in between tags

--- Scripts/app.js
+++ Scripts/app.js
@@ -41,8 +41,28 @@
         });
 
         h2.addEventListener("mouseover", MouseOver);
-
         h2.addEventListener("mouseout", MouseOut);
+
+        //creating a new paragraph element
+        let firstParagraph = document.createElement("p");
+        //configuring the new element
+        firstParagraph.textContent = "This is a very short paragraph!";
+
+        //get a reference to the parentNode of the h2 element
+        let parentDiv = h2.parentNode;
+        
+        //insert the new paragraph before h2 element
+        parentDiv.insertBefore(firstParagraph,h2);
+        
+        //get a reference to the first h1 tag
+        let h1 = document.getElementsByTagName("h1")[0];
+        
+        let anotherParagraph = firstParagraph.cloneNode(true);
+
+         //move the paragraph above the h1 tag
+         h1.parentNode.insertBefore(anotherParagraph,h1);
+
+         parentDiv.removeChild(parentDiv.lastChild);     
 
     }
 
